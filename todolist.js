3fe72eac01a3ad6ed302c77d9e0923d3a4c79423
rabinabaga <rabinabaga@gmail.com>
2024-04-29 21:24:11 +0545

    types of function in js

--- todolist.js
+++ todolist.js
@@ -1,9 +1,19 @@
-let paragraph = document.createElement("p");
-let time = document.getElementById("current-time");
-time.appendChild(paragraph);
 
-setInterval(() => {
-  let currentTime = new Date().toLocaleTimeString();
-  let currentDate = new Date().toLocaleDateString();
-  paragraph.textContent = currentDate + "  " + currentTime;
-}, 1000);
+
+// setInterval(  (a=5,b=3)=>{
+//   console.log(a+b)
+// },1000);
+//  let currentTime;
+// setInterval(, 2000)
+
+const func = function (){
+  currentTime = new Date().toLocaleTimeString();
+  console.log(currentTime);
+}
+
+const func1 = ()=>2+3
+// sum(2,3)
+func()
+console.log(func1());;
+
+
