65e2c4e9fcdb54db967644575265fa9ba17e1d45
rabinabaga <rabinabaga@gmail.com>
2024-04-29 21:38:04 +0545

    task to print dream 1 and dream2 and onwards on every 5 sec

--- todolist.js
+++ todolist.js
@@ -1,19 +1,20 @@
-
-
 // setInterval(  (a=5,b=3)=>{
 //   console.log(a+b)
 // },1000);
 //  let currentTime;
 // setInterval(, 2000)
 
-const func = function (){
-  currentTime = new Date().toLocaleTimeString();
-  console.log(currentTime);
-}
+// const func = function (){
+//   currentTime = new Date().toLocaleTimeString();
+//   console.log(currentTime);
+// }
 
-const func1 = ()=>2+3
-// sum(2,3)
-func()
-console.log(func1());;
-
-
+// const func1 = ()=>2+3
+// // sum(2,3)
+// func()
+// console.log(func1());;
+let i = 0;
+setInterval(() => {
+  i = i + 1;
+  console.log("dream" + " " + i);
+}, 5000);
