75d59de55299aa18268251f2decacee1f5b35f1f
rabinabaga <rabinabaga@gmail.com>
2024-04-30 08:53:10 +0545

    unit conversion using input and button onclick event

--- todolist.js
+++ todolist.js
@@ -1,20 +1,8 @@
-// setInterval(  (a=5,b=3)=>{
-//   console.log(a+b)
-// },1000);
-//  let currentTime;
-// setInterval(, 2000)
+function convertNow(){
+  const result = document.getElementsByClassName("result");
 
-// const func = function (){
-//   currentTime = new Date().toLocaleTimeString();
-//   console.log(currentTime);
-// }
+  const input_value = document.getElementsByClassName("input-units")[0].value;
 
-// const func1 = ()=>2+3
-// // sum(2,3)
-// func()
-// console.log(func1());;
-let i = 0;
-setInterval(() => {
-  i = i + 1;
-  console.log("dream" + " " + i);
-}, 5000);
+  const in_feet = input_value * 3.2808;
+  result[0].textContent = in_feet + " ft";
+}
